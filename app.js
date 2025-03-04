c279097f32fb359a86505f682de073bf911ca333
Elvin251 <emmmdov485@gmail.com>
2025-03-04 20:08:39 +0400

    d
    Co-Authored-By: sabir kral <180758712+sabir-kral@users.noreply.github.com>
    Co-Authored-By: aga2012aaga123 <201779058+aga2012aaga123@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,17 @@
+let form=document.querySelector("form")
+form.addEventListener("submit", (event)=>{
+event.preventDefault();
+let inputs=document.querySelectorAll("input")
+let users=JSON.parse(localStorage.getItem("users"));
+let findusers= users.find(user=> user.username==inputs[0].value&& user.password==inputs[1].value);
+ if (findusers) {
+    alert("giris uqurludur😊")
+    window.location.href="index.html";
+ } else {
+    alert("users tapilmadi😊")
+ }
+
+inputs[0].value="";
+inputs[1].value="";
+
+})
